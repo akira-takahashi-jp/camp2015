4a25b80ef1b7eb29212ef34756c98c21b6329cb6
Akira Takahashi <takahashi@aainc.co.jp>
2015-03-20 23:13:36 +0900

    no message

--- config/database.php
+++ config/database.php
@@ -64,6 +64,18 @@ return [
 			'strict'    => false,
 		],
 
+		'gsdb' => [
+			'driver'    => 'mysql',
+			'host'      => env('DB_HOST', 'localhost'),
+			'database'  => 'gsdb',
+			'username'  => env('DB_USERNAME', 'forge'),
+			'password'  => env('DB_PASSWORD', ''),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+			'strict'    => false,
+		],
+
 		'pgsql' => [
 			'driver'   => 'pgsql',
 			'host'     => env('DB_HOST', 'localhost'),
