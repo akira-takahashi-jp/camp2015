b526f376fbbfd7cfd4b2fb95e678165d4eb904a6
Akira Takahashi <takahashi@aainc.co.jp>
2015-03-21 22:23:44 +0900

    色づけ

--- resources/views/items/report.blade.php
+++ resources/views/items/report.blade.php
@@ -13,6 +13,47 @@
 </div>
 <button type="submit" class="btn btn-default">表示</button>
 
+@if( $item->userGroups )
+<h3>データ</h3>
+<table class="table table-bordered">
+	<tr class="active">
+		<th>ユーザのグループ</th>
+		<th>ユーザ登録</th>
+		@for($i=1; $i<=$request->get('retention_loop'); $i++)
+			<th>{{ $item::$retentionSpanTypeAttr[$item->retention_span_type] . $i }}</th>
+		@endfor
+	</tr>
+	@foreach($item->userGroups as $userGroup )
+	<tr>
+		<td>{{ $userGroup->user_grouping_date }}</td>
+		<td>{{ $userGroup->value }}</td>
+		@foreach($userGroup->retentionDatas as $retentionData)
+			<?php
+			$percentage = $userGroup->value ? round( $retentionData->value/$userGroup->value * 100, 2 ) : false;
+			if($percentage >= 20) $class = 'per_20-';
+			elseif($percentage >= 15) $class = 'per_15-20';
+			elseif($percentage >= 10) $class = 'per_10-15';
+			elseif($percentage >= 5) $class = 'per_5-10';
+			elseif($percentage >= 0.5) $class = 'per_0-5';
+			else $class = 'per_0';
+			?>
+			<td class="{{ $class }}">
+				<p>{{ $percentage ? $percentage . '%' : '-'}}</p>
+				<p>{{ $retentionData->value }}</p>
+			</td>
+		@endforeach
+	</tr>
+	@endforeach
+</table>
+@endif
 
+<style>
+	.per_20-{background-color: #1B39A8; color: #FFFFFF;}
+	.per_15-20{background-color: #3A77E6; color: #FFFFFF;}
+	.per_10-15{background-color: #6B99EC;}
+	.per_5-10{background-color: #9BBBF2;}
+	.per_0-5{background-color: #CEDDF9;}
+	.per_0{background-color: #F6F6F6;}
+</style>
 {!! Form::close() !!}
 @endsection
