e3cd5cbc816d58bae4f76b96063cfd544e33d046
Akira Takahashi <akira.t.0702@gmail.com>
2015-11-11 19:25:27 +0900

    設定

--- config/database.php
+++ config/database.php
@@ -64,31 +64,6 @@ return [
 			'strict'    => false,
 		],
 
-		'gsdb' => [
-			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => 'gsdb',
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-			'strict'    => false,
-		],
-
-		'gsdb_prod' => [
-			'driver'    => 'mysql',
-			'host'      => '127.0.0.1',
-			'port'      => '33060',
-			'database'  => 'gsdb',
-			'username'  => 'root',
-			'password'  => 'allied55',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-			'strict'    => false,
-		],
-
 		'pgsql' => [
 			'driver'   => 'pgsql',
 			'host'     => env('DB_HOST', 'localhost'),
